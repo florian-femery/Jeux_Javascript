bea68d3b627c7f8baeb49af4a33439f59afcc758
florian-femery <florianfemery1@gmail.com>
2024-11-21 20:31:17 +0100

    Player Sprite - Run

--- index.js
+++ index.js
@@ -45,6 +45,16 @@ const player = new Fighter({
   offset: {
     x: 215,
     y: 157
+  },
+  sprite: {
+    idle: {
+      imageSrc: './img/samuraiMack/Idle.png',
+      framesMax: 8
+    },
+    run: {
+      imageSrc: './img/samuraiMack/Run.png',
+      framesMax: 8
+    }
   }
 })
 
@@ -96,10 +106,13 @@ function animate() {
   enemy.velocity.x = 0
 
   // player movement
+  player.image = player.sprite.idle.image
   if (keys.a.pressed && player.lastKey === 'q') {
     player.velocity.x = -5
+    player.image = player.sprite.run.image
   } else if (keys.d.pressed && player.lastKey === 'd') {
     player.velocity.x = 5
+    player.image = player.sprite.run.image
   }
 
   // Enemy movement
--- js/classes.js
+++ js/classes.js
@@ -59,7 +59,8 @@ class Fighter extends Sprite {
         imageSrc,
         scale = 1,
         framesMax = 1,
-        offset = { x: 0, y: 0 }
+        offset = { x: 0, y: 0 },
+        sprites
     }) {
         super({
         position,
@@ -88,12 +89,18 @@ class Fighter extends Sprite {
         this.framesCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 5
+        this.sprites = sprites
+
+        for (const sprite in this.sprites) {
+            sprites[sprite].image = new Image()
+            sprites[sprite].image.src = sprites[sprite].imageSrc
+        }
 }
 
     update() {
         this.draw()
         this.animateFrames()
-        
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
 
