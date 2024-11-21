f394bc18cdc89aca089951913d0011aa89e9d7ff
florian-femery <florianfemery1@gmail.com>
2024-11-21 21:14:51 +0100

    React to Sprite Attacks

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ const player = new Fighter({
       imageSrc: './img/samuraiMack/Attack1.png',
       framesMax: 6
     }
+  },
+  attackBox: {
+    offset: {
+      x: 100,
+      y: 50
+    },
+    width: 160,
+    height: 50
   }
 })
 
@@ -112,6 +120,14 @@ const enemy = new Fighter({
       imageSrc: './img/kenji/Attack1.png',
       framesMax: 4
     }
+  },
+  attackBox: {
+    offset: {
+      x: -170,
+      y: 50
+    },
+    width: 170,
+    height: 50
   }
 })
 
@@ -175,6 +191,7 @@ function animate() {
   } else {
     enemy.switchSprite('idle')
   }
+
   // jumping
   if (enemy.velocity.y < 0) {
     enemy.switchSprite('jump')
@@ -188,11 +205,17 @@ function animate() {
       rectangle1: player,
       rectangle2: enemy
     }) &&
-      player.isAttacking
+      player.isAttacking &&
+      player.framesCurrent === 4
   ) {
     player.isAttacking = false
     enemy.health -= 20
     document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+  }
+
+  // if player misses
+  if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking = false
   }
 
   if (
@@ -200,13 +223,19 @@ function animate() {
       rectangle1: enemy,
       rectangle2: player
     }) &&
-    enemy.isAttacking
+    enemy.isAttacking &&
+    enemy.framesCurrent === 2
   ) {
     enemy.isAttacking = false
     player.health -= 20
     document.querySelector('#playerHealth').style.width = player.health + '%'
   }
 
+  // if player misses
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking = false
+  }
+  
   // end game based on health
   if (enemy.health <= 0 || player.health <= 0) {
     determineWinner({ player, enemy, timerId})
@@ -246,7 +275,7 @@ window.addEventListener('keydown', (event) => {
     case 'ArrowDown':
       enemy.isAttacking = true
       enemy.attack()
-      
+
       break
   }
 })
